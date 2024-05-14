8c27015e71c16a233893a2427c84e81dde37ba24
David Bomba <turbo124@gmail.com>
2024-05-14 16:04:27 +1000

    FACT1 standards

---
+++ src/Writer/Fact1.php
@@ -0,0 +1,360 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace Invoiceninja\Einvoice\Writer;
+
+use DOMElement;
+use Invoiceninja\Einvoice\Writer\Rules\Fact1Rules;
+use Invoiceninja\Einvoice\Writer\Types\CacType;
+use Invoiceninja\Einvoice\Writer\Types\CbcType;
+use Invoiceninja\Einvoice\Writer\Types\ExtType;
+
+class Fact1 extends BaseStandard
+{
+
+    public string $prefix = "xsd";
+
+    private ExtType $extType;
+    private CacType $cacType;
+    private CbcType $cbcType;
+
+    private array $type_tracker = [];
+
+    public string $path = "src/Standards/FACT1/UBL-Invoice-2.1.xsd";
+
+    public string $standard = "FACT1";
+
+        
+    /** @var array $exclusion_nodes - array of nodes to exclude from the output*/
+    private array $exclusion_nodes = [
+        'ext:UBLExtensions',
+    ];
+
+    public function __construct()
+    {
+
+    }
+
+    public function init(): self
+    {
+
+        $this->cbcType = new CbcType();
+        $this->extType = new ExtType();
+        $this->cacType = new CacType();
+
+        $this->document = new \DomDocument();
+        $this->document->load($this->path);
+
+        $this->parentProps()
+        ->childNodes()
+        ->childTypes()
+        ->updateRules()
+        ->write();
+        
+        return $this;
+
+    }
+
+    public function parentProps(): self
+    {
+
+        $elements = $this->document->getElementsByTagName('element');
+
+        $parent_elements = [];
+
+        foreach($elements as $element) {
+
+            if(!$element->hasAttribute('ref') || in_array($element->getAttribute('ref'), $this->exclusion_nodes)) {
+                continue;
+            }
+
+            $parts = explode(":", $element->getAttribute('ref'));
+
+            $maxOccurs = $element->getAttribute('maxOccurs');
+
+            if($maxOccurs == "unbounded") {
+                $maxOccurs = "-1";
+            }
+
+            $parent_elements[$parts[1]] = array_merge($this->stub_validation, [
+                'name' => $parts[1],
+                'base_type' => $element->getAttribute('ref'),
+                'min_occurs' => (int)$element->getAttribute('minOccurs'),
+                'max_occurs' => (int)$maxOccurs,
+                'help' => $this->getAnnotation($element)
+            ]);
+
+        }
+
+        $this->data['InvoiceType'] = [
+            'type' => 'InvoiceType',
+            'help' => '',
+            'choices' => [],
+            'elements' => $parent_elements
+        ];
+
+        return $this;
+    }
+
+    private function getAnnotation(DOMElement $element): string
+    {
+        $result = $this->getXPath("./{$this->prefix}:annotation//{$this->prefix}:documentation//ccts:Component//ccts:Definition", $element);
+
+        return $result->count() > 0 ? trim(str_replace("\n", "", $result->item(0)->nodeValue)) : '';
+
+    }
+
+    private function getXPath(string $path, \DomElement $element = null): ?\DOMNodeList
+    {
+        $xpath = new \DOMXPath($this->document);
+        return $xpath->query($path, $element);
+    }
+
+    private function childNodes(): self
+    {
+        foreach($this->data as $key => $elements) {
+
+            foreach($elements['elements'] as $eKey => $element) {
+                $this->data[$key]['elements'][$eKey] = array_merge($element, $this->harvestNode($element['base_type']));
+            }
+        }
+
+        return $this;
+    }
+
+    private function childTypes(): self
+    {
+        $types = collect();
+
+        foreach($this->data as $key => $elements) {
+
+            foreach($elements['elements'] as $eKey => $element) {
+                if(stripos($element['base_type'], 'Type') !== false) {
+
+                    if(!in_array($element['base_type'], $this->type_tracker)) {
+                        $this->type_tracker[] = $element['base_type'];
+                    }
+
+                    $types->push($element['base_type']);
+                }
+
+            }
+        }
+
+        $child_types = $types->unique()->map(function ($t) {
+
+            foreach($this->cacType->elements as $node) {
+                if($node['type'] == $t) {
+                    return $node;
+                }
+            }
+
+        })->toArray();
+
+        $infants = [];
+
+        foreach($child_types as $infant_type) {
+
+            if(isset($infant_type['elements'])) {
+                foreach($infant_type['elements'] as $e) {
+
+                    if(stripos($e['base_type'], 'Type') !== false && !in_array($e['base_type'], $this->type_tracker)) {
+
+                        foreach($this->cacType->elements as $node) {
+                            if($node['type'] == $e['base_type']) {
+                                $this->type_tracker[] = $e['base_type'];
+                                $infants[$e['name']] = $node;
+                                break;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+        $neonates = [];
+
+        foreach($infants as $neonate) {
+
+            if(isset($neonate['elements'])) {
+                foreach($neonate['elements'] as $e) {
+
+                    if(stripos($e['base_type'], 'Type') !== false && !in_array($e['base_type'], $this->type_tracker)) {
+
+                        foreach($this->cacType->elements as $node) {
+
+                            if($node['type'] == $e['base_type']) {
+
+                                $this->type_tracker[] = $e['base_type'];
+
+                                $neonates[$e['name']] = $node;
+                                break;
+                            }
+                        }
+
+
+                    }
+
+                }
+            }
+
+        }
+
+
+        $foetuses = [];
+
+        foreach($neonates as $foetus) {
+
+            if(isset($foetus['elements'])) {
+                foreach($foetus['elements'] as $e) {
+
+                    if(stripos($e['base_type'], 'Type') !== false && !in_array($e['base_type'], $this->type_tracker)) {
+
+                        foreach($this->cacType->elements as $node) {
+                            if($node['type'] == $e['base_type']) {
+                                $this->type_tracker[] = $e['base_type'];
+                                $foetuses[$e['name']] = $node;
+                                break;
+                            }
+                        }
+
+
+                    }
+
+                }
+            }
+
+        }
+
+        foreach($child_types as $type) {
+
+            $new_set = [];
+            foreach($type['elements'] as $stub) {
+                $new_set[$stub['name']] = $stub;
+            }
+            $type['elements'] = $new_set;
+
+            $this->data[$type['type']] = (object)$type;
+        }
+
+        foreach($infants as $infant) {
+
+            $new_set = [];
+
+            if(isset($infant['elements'])) {
+
+                foreach($infant['elements'] as $key => $stub) {
+                    $new_set[$stub['name']] = $stub;
+                }
+                $infant['elements'] = $new_set;
+
+            }
+
+            $this->data[$infant['type']] = (object)$infant;
+
+        }
+
+        foreach($neonates as $neonate) {
+
+            $new_set = [];
+            if(isset($neonate['elements'])) {
+
+                foreach($neonate['elements'] as $key => $stub) {
+                    $new_set[$stub['name']] = $stub;
+                }
+                $neonate['elements'] = $new_set;
+            }
+
+            $this->data[$neonate['type']] = (object)$neonate;
+        }
+
+        foreach($foetuses as $foetus) {
+
+            $new_set = [];
+
+            if(isset($foetus['elements'])) {
+                foreach($foetus['elements'] as $key => $stub) {
+                    $new_set[$stub['name']] = $stub;
+                }
+                $foetus['elements'] = $new_set;
+            }
+
+            $this->data[$foetus['type']] = (object)$foetus;
+        }
+
+        return $this;
+    }
+
+
+    private function harvestNode(string $name)
+    {
+
+        $parts = explode(":", $name);
+
+        match($parts[0]) {
+            'cac' => $type = $this->cacType,
+            'cbc' => $type = $this->cbcType,
+            'ext' => $type = $this->extType,
+        };
+
+        return $type->getNamedType($parts[1]);
+
+    }
+
+    private function updateRules(): self
+    {
+
+        $e = new Fact1Rules();
+        $rules = $e->buildInvoice();
+
+        foreach($rules["invoice"] as $key => $value) {
+
+            foreach($this->data["InvoiceType"]['elements'] as $eKey => $eValue) {
+
+                if(isset($eValue['name']) && $eValue['name'] == $key) {
+
+                    $this->data["InvoiceType"]['elements'][$eKey] = array_merge($eValue, $value);
+
+                }
+            }
+
+        }
+
+        foreach($rules['nested'] as $key => $value) {
+
+            foreach($this->data as $dKey => $dValue) {
+                if(isset($dValue->type) && $key == $dValue->type) {
+                    foreach($rules['nested'][$key] as $nestKey => $value) {
+                        foreach($dValue->elements as $ddKey => $ddValue) {
+                            if(is_array($ddValue) && $ddValue['name'] == $nestKey) {
+
+                                $this->data[$dKey]->elements[$ddKey] = (object)array_merge((array)$this->data[$dKey]->elements[$ddKey], $value);
+                            } elseif(is_object($ddValue) && $ddValue->name == $nestKey) {
+
+                                $this->data[$dKey]->elements[$ddKey] = (object)array_merge((array)$this->data[$dKey]->elements[$ddKey], $value);
+
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        return $this;
+    }
+
+}
---
+++ src/Writer/Rules/Fact1Rules.php
@@ -0,0 +1,324 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+namespace Invoiceninja\Einvoice\Writer\Rules;
+
+class Fact1Rules
+{
+    public array $stub_validation = [
+        "name" => null,
+        "base_type" => null,
+        "resource" => [],
+        "max_length" => null,
+        "min_length" => null,
+        "pattern" => null,
+    ];
+
+    private $build_array = [];
+
+    public function __construct()
+    {
+
+    }
+
+    public function getBuildArray(): array
+    {
+        return $this->build_array;
+    }
+
+    public function setBuildArray(array $array): self
+    {
+        $this->build_array = array_merge($this->build_array, $array);
+
+        return $this;
+    }
+
+    public function buildInvoice(): array
+    {
+        $this->build_array = [];
+
+        $invoice =
+
+        $this->setBuildArray($this->invoiceRules())
+             ->setBuildArray($this->genericEntityRules())
+             ->getBuildArray();
+
+        return ['invoice' => $invoice, "nested" => $this->complexRules()];
+    }
+
+    public function ruleSet(): array
+    {
+        $rules = [
+            [
+                'name' => 'InvoiceTypeCode',
+                'base_type' => 'string',
+                'resource' => ['380','384','389','751'],
+            ],
+            [
+                'name' => 'StreetName',
+                'base_type' => 'string',
+                'min_length' => 1,
+            ],
+            [
+                'name' => 'CityName',
+                'base_type' => 'string',
+                'min_length' => 1,
+            ],
+            [
+                'name' => 'DescriptionCode',
+                'base_type' => 'string',
+                'resource' => ['3', '35', '432'],
+            ],
+            [
+                'name' => 'CreditTypeCode',
+                'base_type' => 'string',
+                'resource' => ['381'],
+            ],
+            [
+                'name' => 'Note',
+                'base_type' => 'string',
+                'max_length' => 300,
+            ],
+        ];
+
+        foreach($rules as $key => $rule) {
+            $rules[$key] = array_merge($this->stub_validation, $rule);
+        }
+
+        return $rules;
+    }
+
+    public function complexRules()
+    {
+        return [
+            'PeriodType' => [
+                'DescriptionCode' => [
+                    'resource' => [
+                        '3' => "Invoice Date",
+                        '35' => "Delivery Date",
+                        '432' => "Payment Date",
+                    ],
+                ],
+            ],
+            'AddressType' => [
+                'AdditionalStreetName' => [
+                    'max_length' => 100,
+                ],
+                'StreetName' => [
+                    'min_length' => 1,
+                ],
+                'CityName' => [
+                    'min_length' => 1,
+                    'max_length' => 50,
+                ],
+                'PostalZone' => [
+                    'max_length' => 20,
+                ],
+                'CountrySubentity' => [
+                    'base_type' => 'string',
+                    'min_length' => 1,
+                    'resource' => ['RO-AB','RO-AG','RO-AR','RO-B','RO-BC','RO-BH','RO-BN','RO-BR','RO-BT','RO-BV','RO-BZ','RO-CJ','RO-CL','RO-CS','RO-CT', 'RO-CV', 'RO-DB', 'RO-DJ', 'RO-GJ', 'RO-GL', 'RO-GR', 'RO-HD', 'RO-HR' , 'RO-IF', 'RO-IL', 'RO-IS', 'RO-MH', 'RO-MM', 'RO-MS', 'RO-NT', 'RO-OT', 'RO-PH', 'RO-SB', 'RO-SJ', 'RO-SM', 'RO-SV', 'RO-TL', 'RO-TM', 'RO-TR', 'RO-VL', 'RO-VN', 'RO-VS'],
+                ],
+            ],
+            'DocumentReferenceType' => [
+                'ID' => [
+                    'min_length' => 1,
+                    'max_length' => 200,
+                ],
+                'DocumentDescription' => [
+                    'max_length' => 100,
+                ],
+            ],
+            'OrderReferenceType' => [
+                'ID' => [
+                    'max_length' => 200,
+                ],
+                'SalesOrderID' => [
+                    'max_length' => 200,
+                ],
+            ],
+            'InvoiceLineType' => [
+                'AccountingCost' => [
+                    'max_length' => 100,
+                ],
+                'Note' => [
+                    'max_length' => 300,
+                ],
+                'LineExtensionAmount' => [
+                    'fraction_digits' => 2,
+                ]
+            ],
+            'AddressLineType' => [
+                'Line' => [
+                    'max_length' => 100,
+                ]
+            ],
+            'ContactType' => [
+                'Name' => [
+                    'max_length' => 100,
+                ],
+                'Telephone' => [
+                    'max_length' => 100,
+                ],
+                'ElectronicMail' => [
+                    'max_length' => 100,
+                ],
+            ],
+            'TaxCategoryType' => [
+                'TaxExemptionReason' => [
+                    'max_length' => 100,
+                ]
+            ],
+            'PartyLegalEntityType' => [
+                'RegistrationName' => [
+                    'max_length' => 200,
+                ],
+                'CompanyLegalForm' => [
+                    'max_length' => 1000,
+                ]
+            ],
+            'PartyNameType' => [
+                'Name' => [
+                    'max_length' => 200,
+                ],
+            ],
+            'PaymentTermsType' => [
+                'Note' => [
+                    'max_length' => 300,
+                ],
+            ],
+            'FinancialAccountType' => [
+                'Name' => [
+                    'max_length' => 200,
+                ],
+            ],
+            'CardAccountType' => [
+                'HolderName' => [
+                    'max_length' => 200,
+                ],
+            ],
+            'PaymentMeansType' => [
+                'PaymentID' => [
+                    'max_length' => 140,
+                ]
+            ],
+            'ItemType' => [
+                'Name' => [
+                    'max_length' => 100,
+                ],
+                'Description' => [
+                    'max_length' => 200,
+                ],
+                'AdditionalItemProperty' => [
+                    'max_occurs' => 50,
+                ],
+            ],
+            'ItemPropertyType' => [
+                'Name' => [
+                    'max_length' => 50,
+                ],
+                'Value' => [
+                    'max_length' => 100,
+                ]
+            ],
+            'AllowanceChargeType' => [
+                'AllowanceChargeReasonCode' => [
+                    'max_length' => 100,
+                ],
+                'Amount' => [
+                    'fraction_digits' => 2
+                ],
+                'BaseAmount' => [
+                    'fraction_digits' => 2
+                ],
+                'AllowanceChargeReason' => [
+                    'max_length' => 100
+                ]
+            ],
+            'MonetaryTotalType' => [
+                'LineExtensionAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'AllowanceTotalAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'ChargeTotalAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'TaxExclusiveAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'TaxInclusiveAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'PrepaidAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'PayableRoundingAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'PayableAmount' => [
+                    'fraction_digits' => 2,
+                ],
+            ],
+            'TaxSubtotalType' => [
+                'TaxableAmount' => [
+                    'fraction_digits' => 2,
+                ],
+                'TaxAmount' => [
+                    'fraction_digits' => 2,
+                ],
+            ],
+
+        ];
+    }
+
+    public function invoiceRules()
+    {
+        return [
+            'InvoiceTypeCode' => [
+                'base_type' => 'string',
+                'resource' => ['380','384','389','751'],
+            ],
+        ];
+    }
+
+    public function creditRules()
+    {
+        return [
+            'CreditTypeCode' => [
+                'base_type' => 'string',
+                'resource' => ['381'],
+            ],
+        ];
+    }
+
+    public function genericEntityRules()
+    {
+        return [
+            'ID' => [
+                'base_type' => 'string',
+                'pattern' => '([0-9])',
+            ],
+            'Note' => [
+                'base_type' => 'string',
+                'min_length' => 0,
+                'max_length' => 300,
+            ],
+            'InvoiceDocumentReference' => [
+                'base_type' => 'string',
+                'min_length' => 1,
+                'max_length' => 200,
+            ],
+        ];
+    }
+
+}

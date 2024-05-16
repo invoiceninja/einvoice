4a842e69c283e43e0d428d2f34d6f8a8b40544dc
David Bomba <turbo124@gmail.com>
2024-05-16 23:23:11 +1000

    Updates for commands

---
+++ src/Command/ModelCommand.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Invoice Ninja (https://invoiceninja.com).
+ *
+ * @link https://github.com/invoiceninja/invoiceninja source repository
+ *
+ * @copyright Copyright (c) 2024. Invoice Ninja LLC (https://invoiceninja.com)
+ *
+ * @license https://www.elastic.co/licensing/elastic-license
+ */
+
+declare(strict_types=1);
+
+namespace Invoiceninja\Einvoice\Command;
+
+use Invoiceninja\Einvoice\Writer\Generator\Generator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'model:create',
+    description: 'Create Model Classes',
+    hidden: false,
+    aliases: ['model:create']
+)]
+final class ModelCommand extends Command
+{
+
+
+    public $output;
+
+    protected function configure()
+    {
+    }
+
+    /**
+     * Here all logic happens
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->output = $output;
+
+            $generator = new Generator();
+            $generator->build();
+            
+        // return value is important when using CI, to fail the build when the command fails
+        // in case of fail: "return self::FAILURE;"
+        return self::SUCCESS;
+    }
+
+}

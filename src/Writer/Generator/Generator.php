31aa753ec9f3ca652cad539cc7e37d85e33ccfb6
David Bomba <turbo124@gmail.com>
2024-05-16 22:11:14 +1000

    Cleanup for class generation

--- src/Writer/Generator/Generator.php
+++ src/Writer/Generator/Generator.php
@@ -11,15 +11,12 @@
 
 namespace Invoiceninja\Einvoice\Writer\Generator;
 
-use DOMDocument;
 use Nette\PhpGenerator\Type;
 use Spatie\LaravelData\Data;
-use Nette\PhpGenerator\Printer;
 use Spatie\LaravelData\Optional;
 use Nette\PhpGenerator\ClassType;
 use Illuminate\Support\Collection;
 use Nette\PhpGenerator\PhpNamespace;
-use Nette\PhpGenerator\Property;
 
 class Generator
 {
@@ -30,19 +27,18 @@ class Generator
         'FatturaPA'
     ];
 
-    private Collection $child_classes;
+    public Collection $child_classes;
 
-    private Collection $document;
+    public Collection $document;
 
-    private string $namespace = "Invoiceninja\Einvoice\Models\\";
+    public string $namespace = "Invoiceninja\Einvoice\Models\\";
 
-    private string $standard = "";
+    public string $standard = "";
 
-    private string $write_path = "src/Models/";
+    public string $write_path = "src/Models/";
 
     public function __construct()
     {
-
     }
 
     public function build()
@@ -61,17 +57,6 @@ class Generator
 
         });
 
-        $this->child_classes->each(function ($child){
-
-            $name = $child['name'];
-
-            //check if subclass! then we need to nest a reference the correct namespace!!
-
-            if(!file_exists("{$this->write_path}/{$this->standard}/{$name}.php"))
-                $this->writeNette($child['name'], $this->getChildType($child['base_type']), $child['base_type']);
-        
-        });
-
     }
 
     public function getChildType(string $name)
@@ -79,7 +64,7 @@ class Generator
         return $this->document[$name];
     }
 
-    private function resolveType(string $type): string
+    public function resolveType(string $type): string
     {
         return match($type){
             'integer' => $type = 'int',
@@ -93,51 +78,51 @@ class Generator
 
     }
 
-    public function writeNette(string $name, mixed $type, ?string $subclass = '')
+    public function writeNette(string $name, mixed $type)
     {
-        $name_space_string = $this->namespace.$this->standard;
+        echo "nette => {$name}".PHP_EOL;
 
-        if(strlen($subclass) > 2)
-            $name_space_string.= "\\".$subclass;
-
-        $namespace = new PhpNamespace($name_space_string);
+        $namespace = new PhpNamespace($this->namespace.$this->standard);
         $namespace->addUse(Data::class);
-        $namespace->addUse(Optional::class);
+        
 
         $class = new ClassType($name);
         $class->setExtends(Data::class);
-        $namespace->add($class);
 
         foreach($type['elements'] as $key => $element) {
 
             if($name == $element['name'])
                 continue;
 
+            //if it has a type, resolve now, write it, and then continue!
 
-
-$standard_type_path = $element['name'];
-// $standard_type_path = $element['base_type']."\\".$element['name'];
-// standard_type_path = $this->namespace.$this->standard."\\".$element['base_type']."\\".$element['name'];
-
-            $base_type = stripos($element['base_type'], 'Type') !== false ? $standard_type_path : $this->resolveType($element['base_type']);
-
-
-            if($base_type == $standard_type_path && strlen($subclass) > 2)
+            if(stripos($element['base_type'], 'Type') !== false ){
+                $type_object = $this->writeBaseType($element['name'], $element['base_type']);
                 $namespace->addUse($this->namespace.$this->standard."\\".$element['base_type']."\\".$element['name']);
-            elseif($base_type == $standard_type_path )
-                $namespace->addUse($this->namespace.$this->standard."\\".$element['name']);
+            }
 
-            $type = $element['min_occurs'] == 0 ? Type::union($base_type, Optional::class) : $base_type;
+            // $standard_type_path = $this->namespace.$this->standard."\\".$element['base_type']."\\".$element['name'];
+            $base_type = stripos($element['base_type'], 'Type') !== false ? $this->namespace.$this->standard."\\".$element['base_type']."\\".$element['name'] : $this->resolveType($element['base_type']);
+
+            
+            if($element['min_occurs'] == 0){
+                
+                $namespace->addUse(Optional::class);
+                $type = Type::union($base_type, Optional::class);
+
+            }
+            else {    
+                $type = $base_type;
+            } 
 
             $class->addProperty($element['name'])
-                ->setPublic() // or setVisibility('private')
+                ->setPublic() 
                 ->setType($type);
 
-            if(stripos($element['base_type'], 'Type') !== false)
-                $this->child_classes->push(['name' => $element['name'], 'base_type' => $element['base_type']]);
+            // if(stripos($element['base_type'], 'Type') !== false)
+            //     $this->child_classes->push($element);
 
             if(count($element['resource']) > 0){
-                // echo $element['name']."_array".PHP_EOL;
                 $class->addProperty($element['name']."_array")
                       ->setPublic()
                       ->setType('array')
@@ -147,27 +132,40 @@ $standard_type_path = $element['name'];
 
         $namespace->add($class);
 
-        $printer = new Printer();
+        $name = str_replace("Type","",$name);
+        $this->write($namespace, "{$this->write_path}{$this->standard}/{$name}.php");
+        
+        //check for addition child types to write NOW
+    }
 
+    private function writeBaseType($name, $base_type): \stdClass
+    {
+        echo "writing {$name}".PHP_EOL;
+
+        $type_generator = new TypeGenerator($this, $name, $base_type);
+        
+        return $type_generator->init();
+
+    }
+
+
+    public function write($namespace, $path)
+    {
+                
         $class_print = "<?php ". self::LINE_FEED . self::LINE_FEED;
         $class_print .= $namespace;
-        // $class_print .= $printer->printClass($class); // same as: echo $class
-        $file_path = "{$this->write_path}{$this->standard}/";
 
-        if(strlen($subclass) > 1){
-            
-            $file_path .= "{$subclass}/";
-            if(!is_dir($file_path))
-                mkdir($file_path);
+        $pathinfo = pathinfo($path);
 
-        }
+        echo print_r($path).PHP_EOL;
 
-        $file_path .= "{$name}.php";
+        if(!is_dir($pathinfo['dirname']))
+            mkdir($pathinfo['dirname']);
 
-        $fp = fopen($file_path, 'w');
+        $fp = fopen($path, 'w');
         fwrite($fp, $class_print);
         fclose($fp);
 
-
     }
+
 }

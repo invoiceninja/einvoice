8ce5e3d1b70ac6e4a417da0d1752bf8dfb477fa5
David Bomba <turbo124@gmail.com>
2024-05-21 20:01:09 +1000

    Upgrade laravel-data

--- src/Models/Transformers/DataCollectionTransformer.php
+++ src/Models/Transformers/DataCollectionTransformer.php
@@ -2,16 +2,36 @@
 
 namespace Invoiceninja\Einvoice\Models\Transformers;
 
+use Spatie\LaravelData\Casts\Cast;
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Transformers\Transformer;
+use Spatie\LaravelData\Support\Creation\CreationContext;
+use Spatie\LaravelData\Support\Transformation\TransformationContext;
 
-class DataCollectionTransformer implements Transformer
+class DataCollectionTransformer implements Cast,Transformer
 {
-    public function __construct(private ?int $precision = 2)
+    public function __construct(private ?bool $ignore = false)
     {
-    }
+    }    
 
-    public function transform(DataProperty $property, mixed $value): mixed
+    public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): string
+    {
+        if($this->ignore)
+            return $property->type->dataClass::collection($value);
+
+            $coll = [];
+
+            foreach($value as $array) {
+                $coll[] = $property->type->dataClass::from($array);
+            }
+            
+            $y = $property->type->dataClass::collection($coll);
+
+            return $y;
+
+    }
+    
+    public function transform(DataProperty $property, mixed $value, TransformationContext $context): string
     {
         return count($value) <= 1 ? [$value] : $value;
     }
--- src/Models/Transformers/FloatTransformer.php
+++ src/Models/Transformers/FloatTransformer.php
@@ -4,13 +4,15 @@ namespace Invoiceninja\Einvoice\Models\Transformers;
 
 use Spatie\LaravelData\Support\DataProperty;
 use Spatie\LaravelData\Transformers\Transformer;
+use Spatie\LaravelData\Support\Transformation\TransformationContext;
+
 
 class FloatTransformer implements Transformer
 {
 
     public function __construct(private ?int $precision = 2){}
 
-    public function transform(DataProperty $property, mixed $value): mixed
+    public function transform(DataProperty $property, mixed $value, TransformationContext $context): mixed
     {
         return number_format($value, $this->precision);
     }

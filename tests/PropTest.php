12cd5b7b9f7db00545777ffa25e37c59047d692c
David Bomba <turbo124@gmail.com>
2024-05-31 11:05:57 +1000

    Minor fixes

--- tests/PropTest.php
+++ tests/PropTest.php
@@ -26,7 +26,63 @@ class PropTest extends TestCase
                 continue;
             }
 
-            echo $element->getAttribute('ref').PHP_EOL;
+            // echo $element->getAttribute('ref').PHP_EOL;
+        }
+    }
+
+    public function testFatturaProps()
+    {
+        $dom = new DOMDocument();
+        $dom->load("src/Standards/FatturaPA/Schema_del_file_xml_FatturaPA_v1.2.2.xsd");
+
+
+// $complexTypes = $dom->getElementsByTagName('element');
+        $complexTypes = $dom->getElementsByTagName('complexType');
+
+        foreach($complexTypes as $type) {
+
+            if($type->hasAttribute('name'))
+                echo $type->getAttribute('name').PHP_EOL;
         }
     }
 }
+
+
+
+// DatiTrasmissione' => ,
+// IdFiscale' => ,
+// ContattiTrasmittente' => ,
+// DatiGenerali' => ,
+// DatiGeneraliDocumento' => ,
+// DatiRitenuta' => ,
+// DatiBollo' => ,
+// DatiCassaPrevidenziale' => ,
+// ScontoMaggiorazione' => ,
+// DatiSAL' => ,
+// DatiDocumentiCorrelati' => ,
+// DatiDDT' => ,
+// DatiTrasporto' => ,
+// Indirizzo' => ,
+// FatturaPrincipale' => ,
+// CedentePrestatore' => ,
+// DatiAnagraficiCedente' => ,
+// Anagrafica' => ,
+// DatiAnagraficiVettore' => ,
+// IscrizioneREA' => ,
+// Contatti' => ,
+// RappresentanteFiscale' => ,
+// DatiAnagraficiRappresentante' => ,
+// CessionarioCommittente' => ,
+// RappresentanteFiscaleCessionario' => ,
+// DatiAnagraficiCessionario' => ,
+// DatiBeniServizi' => ,
+// DatiVeicoli' => ,
+// DatiPagamento' => ,
+// DettaglioPagamento' => ,
+// TerzoIntermediarioSoggettoEmittente' => ,
+// DatiAnagraficiTerzoIntermediario' => ,
+// Allegati' => ,
+// DettaglioLinee' => ,
+// CodiceArticolo' => ,
+// AltriDatiGestionali' => ,
+// DatiRiepilogo' => ,

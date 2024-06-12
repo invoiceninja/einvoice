82dc3456cfb8e0b6cc5de79a971d61b4cc2fa4a5
David Bomba <turbo124@gmail.com>
2024-06-12 12:51:01 +1000

    Updated Peppol classes

---
+++ src/Models/Peppol/MeasureType/LatitudeMinutesMeasure.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace InvoiceNinja\EInvoice\Models\Peppol\MeasureType;
+
+use DateTime;
+use DateTimeInterface;
+use InvoiceNinja\EInvoice\Models\Normalizers\DecimalPrecision;
+use Symfony\Component\Serializer\Attribute\Context;
+use Symfony\Component\Serializer\Attribute\SerializedName;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Validator\Constraints\All;
+use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Valid;
+
+class LatitudeMinutesMeasure
+{
+	/** @var string */
+	#[SerializedName('#')]
+	public string $value;
+
+	/** @var string */
+	#[SerializedName('@unitCode')]
+	public string $unitCode;
+
+	/** @var string */
+	#[SerializedName('@unitCodeListVersionID')]
+	public string $unitCodeListVersionID;
+}
